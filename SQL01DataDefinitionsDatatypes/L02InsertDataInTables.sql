611cd3ff6be41cfd1d72232a112d8de8d3fb4236
tproykov <tproykov@gmail.com>
2024-08-31 17:59:47 +0100

    insert initial files in repository

---
+++ SQL01DataDefinitionsDatatypes/L02InsertDataInTables.sql
@@ -0,0 +1,6 @@
+INSERT INTO employees (first_name, last_name)
+VALUES ("Ivan", "Ivanov");
+INSERT INTO employees (first_name, last_name)
+VALUES ("Todor", "Todorov");
+INSERT INTO employees (first_name, last_name)
+VALUES ("Petar", "Iliev");
