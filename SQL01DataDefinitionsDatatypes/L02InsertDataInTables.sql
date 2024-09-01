967f8f037121741153a652ae94abcb10f2dae14b
tproykov <tproykov@gmail.com>
2024-09-01 21:15:00 +0100

    insert data in tables

--- SQL01DataDefinitionsDatatypes/L02InsertDataInTables.sql
+++ SQL01DataDefinitionsDatatypes/L02InsertDataInTables.sql
@@ -1,6 +1,7 @@
+USE default_database;
 INSERT INTO employees (first_name, last_name)
-VALUES ("Ivan", "Ivanov");
+VALUES ('Ivan', 'Ivanov');
 INSERT INTO employees (first_name, last_name)
-VALUES ("Todor", "Todorov");
+VALUES ('Todor', 'Todorov');
 INSERT INTO employees (first_name, last_name)
-VALUES ("Petar", "Iliev");
+VALUES ('Petar', 'Iliev');
