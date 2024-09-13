3b75ab3ef08b77a9f1b7117c2a95779324689bcc
Todor Proykov <tproykov@gmail.com>
2024-09-13 16:55:47 +0300

    Update E13BasicInsert.sql

--- SQL01DataDefinitionsDatatypes/E13BasicInsert.sql
+++ SQL01DataDefinitionsDatatypes/E13BasicInsert.sql
@@ -4,15 +4,31 @@ CREATE TABLE towns (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL
 );
+INSERT INTO towns (name) VALUES
+    ('Sofia'),
+    ('Plovdiv'),
+    ('Varna'),
+    ('Burgas');
+
 CREATE TABLE addresses (
     id INT PRIMARY KEY AUTO_INCREMENT,
     address_text VARCHAR(100) NOT NULL,
-    town_id INT
+    town_id INT NOT NULL,
+    CONSTRAINT fk__town_id__addresses__id__towns
+        FOREIGN KEY (town_id) REFERENCES towns(id)
 );
+
 CREATE TABLE departments (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL
 );
+INSERT INTO departments (name) VALUES
+    ('Engineering'),
+    ('Sales'),
+    ('Marketing'),
+    ('Software Development'),
+    ('Quality Assurance');
+
 CREATE TABLE employees (
     id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
@@ -21,6 +37,16 @@ CREATE TABLE employees (
     job_title VARCHAR(50) NOT NULL,
     department_id INT,
     hire_date DATE,
-    salary INT,
-    address_id INT
+    salary DECIMAL(10, 2) NOT NULL,
+    address_id INT,
+    CONSTRAINT fk__department_id__employees__id__departments
+        FOREIGN KEY (department_id) REFERENCES departments(id),
+    CONSTRAINT fk__address_id__employees__id__addresses
+        FOREIGN KEY (address_id) REFERENCES addresses(id)
 );
+INSERT INTO employees (first_name, middle_name, Last_name, job_title, department_id, hire_date, salary, address_id) VALUES
+        ('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013-02-01', 3500.00, NULL),
+        ('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004-03-02', 4000.00, NULL),
+        ('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016-08-28', 525.25, NULL),
+        ('Georgi', 'Terziev', 'Ivanov', 'CEO', 2, '2007-12-09', 3000.00, NULL),
+        ('Peter', 'Pan', 'Pan', 'Intern', 3, '2016-08-28', 599.88, NULL);
