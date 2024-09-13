4d710cb02ff1cfb624679aa464f9eec0fd422f23
Todor Proykov <tproykov@gmail.com>
2024-09-13 22:34:54 +0300

    Update E09FindNamesOFAllEmployees.sql

--- SQL02BasicCRUD/E09FindNamesOFAllEmployees.sql
+++ SQL02BasicCRUD/E09FindNamesOFAllEmployees.sql
@@ -1,4 +1,4 @@
 USE soft_uni;
 SELECT CONCAT_WS(' ', first_name, middle_name, last_name) AS `Full Name`
 FROM employees
-WHERE salary = 25000 OR salary = 14000 OR salary = 12500 OR salary = 23600;
+WHERE salary IN (25000, 14000, 12500, 23600);
