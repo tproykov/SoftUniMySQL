1d96bb743e6288a12957b8f34d91bc2f64525317
Todor Proykov <tproykov@gmail.com>
2024-09-14 23:39:45 +0300

    contains multiple A's

---
+++ SQL03BuiltInFunctions/E10CountriesHoldingA.sql
@@ -0,0 +1,7 @@
+USE geography;
+SELECT country_name, iso_code
+FROM countries
+WHERE
+    CHAR_LENGTH(country_name) -
+    CHAR_LENGTH(REPLACE(LOWER(country_name), 'a', '')) >= 3
+ORDER BY iso_code;
