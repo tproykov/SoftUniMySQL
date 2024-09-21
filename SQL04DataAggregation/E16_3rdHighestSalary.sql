9875554cbbaabfd1d5016261eea60b7dabc87035
tproykov <tproykov@gmail.com>
2024-09-21 13:24:03 +0100

    Update E16_3rdHighestSalary.sql

--- SQL04DataAggregation/E16_3rdHighestSalary.sql
+++ SQL04DataAggregation/E16_3rdHighestSalary.sql
@@ -2,9 +2,9 @@ USE soft_uni;
 SELECT DISTINCT department_id,
                 (SELECT DISTINCT salary
                  FROM employees AS e1
-                 WHERE e1.department_id = employees.department_id
+                 WHERE e1.department_id = e.department_id
                  ORDER BY salary DESC
                  LIMIT 1 OFFSET 2) AS `third_highest_salary`
-FROM employees
+FROM employees AS e
 HAVING third_highest_salary IS NOT NULL
 ORDER BY department_id;
