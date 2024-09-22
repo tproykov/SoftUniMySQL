da13c4986d44a9a64453acd3caca80b5bf6b915b
tproykov <tproykov@gmail.com>
2024-09-22 08:48:59 +0100

    Update L01Managers.sql

--- SQL06SubqueriesAndJOINs/L01Managers.sql
+++ SQL06SubqueriesAndJOINs/L01Managers.sql
@@ -1,11 +1,11 @@
 USE soft_uni;
 SELECT
-    DISTINCT e.manager_id AS employee_id,
+    DISTINCT d.manager_id AS employee_id,
     CONCAT(e.first_name, ' ', e.last_name) AS full_name,
-    e.department_id,
+    d.department_id,
     d.name AS department_name
-FROM employees AS e JOIN departments AS d ON
-    e.department_id = d.department_id
+FROM departments AS d JOIN employees AS e ON
+    d.manager_id = e.employee_id
 ORDER BY employee_id
 LIMIT 5;
 
