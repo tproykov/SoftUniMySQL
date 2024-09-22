89acd38c05519f4cf50b11b61bfc39b6945047f9
tproykov <tproykov@gmail.com>
2024-09-22 10:00:00 +0100

    Update E03salesEmployee.sql

---
+++ SQL06SubqueriesAndJOINs/E03salesEmployee.sql
@@ -0,0 +1,10 @@
+USE soft_uni;
+SELECT
+    e.employee_id,
+    e.first_name,
+    e.last_name,
+    d.name AS department_name
+FROM employees AS e
+JOIN departments AS d ON e.department_id = d.department_id
+WHERE d.name LIKE 'Sales'
+ORDER BY employee_id DESC;
