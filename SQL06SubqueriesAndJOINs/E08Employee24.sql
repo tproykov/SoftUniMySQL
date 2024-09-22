b4a3a28ea3a21fc94ca14851e818e670cd5519e6
tproykov <tproykov@gmail.com>
2024-09-22 14:00:35 +0100

    exercise 8

---
+++ SQL06SubqueriesAndJOINs/E08Employee24.sql
@@ -0,0 +1,14 @@
+USE soft_uni;
+SELECT
+    e.employee_id,
+    e.first_name,
+    IF (YEAR(p.start_date) >= 2005, NULL, p.name) AS project_name
+FROM employees AS e
+    JOIN employees_projects AS ep
+        ON e.employee_id = ep.employee_id
+    JOIN projects AS p
+        ON ep.project_id = p.project_id
+WHERE e.employee_id = 24
+ORDER BY project_name;
+
+
