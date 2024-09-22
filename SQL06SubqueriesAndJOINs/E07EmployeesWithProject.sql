cf886a85274dda67fd29b8bcbe45d48fd439a4fe
tproykov <tproykov@gmail.com>
2024-09-22 13:45:22 +0100

    exercise 7

---
+++ SQL06SubqueriesAndJOINs/E07EmployeesWithProject.sql
@@ -0,0 +1,13 @@
+USE soft_uni;
+SELECT
+    e.employee_id,
+    e.first_name,
+    p.name AS project_name
+FROM employees AS e
+    JOIN employees_projects AS ep
+        ON e.employee_id = ep.employee_id
+    JOIN projects AS p
+        ON ep.project_id = p.project_id
+WHERE DATE(start_date) > '2002-08-13' AND end_date IS NULL
+ORDER BY first_name, name
+LIMIT 5;
