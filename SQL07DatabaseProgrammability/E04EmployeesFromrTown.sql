4f7a7a2968cb7c7c4325cfa3c9b0e1bb8a7c6e59
tproykov <tproykov@gmail.com>
2024-09-28 17:36:57 +0100

    exercise 4

---
+++ SQL07DatabaseProgrammability/E04EmployeesFromrTown.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE PROCEDURE usp_get_employees_from_town(town_name VARCHAR(50))
+BEGIN
+    SELECT first_name, last_name
+    FROM employees AS e
+    JOIN addresses AS a ON e.address_id = a.address_id
+    JOIN towns AS t ON a.town_id = t.town_id
+    WHERE t.name LIKE town_name
+    ORDER BY first_name, last_name, employee_id;
+END $$
+DELIMITER ;
