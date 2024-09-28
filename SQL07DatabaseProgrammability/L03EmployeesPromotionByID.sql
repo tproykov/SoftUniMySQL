43350eea5998c12f4bcd50f15b60e0f2ff301c9d
tproykov <tproykov@gmail.com>
2024-09-28 16:08:10 +0100

    exercise 3

---
+++ SQL07DatabaseProgrammability/L03EmployeesPromotionByID.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE PROCEDURE usp_raise_salary_by_id(id INT)
+BEGIN
+    START TRANSACTION;
+    IF ((SELECT COUNT(employee_id) FROM employees WHERE employee_id LIKE id) = 0)
+        THEN ROLLBACK;
+    ELSE
+        UPDATE employees SET salary = salary * 1.05 WHERE employee_id = id;
+    END IF;
+END $$
+DELIMITER ;
