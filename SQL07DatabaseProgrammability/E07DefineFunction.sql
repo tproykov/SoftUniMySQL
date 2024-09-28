3e962f774899b87bb46474469b7041196f162521
tproykov <tproykov@gmail.com>
2024-09-28 22:13:45 +0100

    exercise 7

---
+++ SQL07DatabaseProgrammability/E07DefineFunction.sql
@@ -0,0 +1,8 @@
+DELIMITER $$
+CREATE FUNCTION ufn_is_word_comprised(set_of_letters varchar(50), word varchar(50))
+RETURNS TINYINT
+DETERMINISTIC
+BEGIN
+    RETURN word REGEXP (CONCAT('^[', set_of_letters, ']+$'));
+END $$
+DELIMITER ;
