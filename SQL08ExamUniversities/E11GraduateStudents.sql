4d75149cd46d5237d076d176728127eb6f7e6aae
tproykov <tproykov@gmail.com>
2024-09-30 22:23:59 +0100

    task 11 from the exam

--- SQL08ExamUniversities/E11GraduateStudents.sql
+++ SQL08ExamUniversities/E11GraduateStudents.sql
@@ -1,9 +1,12 @@
 DELIMITER $$
 CREATE PROCEDURE udp_graduate_all_students_by_year(year_started INT)
 BEGIN
-
-
-
-
+    UPDATE students AS s
+        JOIN students_courses AS sc ON s.id = sc.student_id
+        JOIN courses AS c ON sc.course_id = c.id
+    SET s.is_graduated = 1
+    WHERE YEAR(c.start_date) = year_started AND s.is_graduated = 0;
 END $$
 DELIMITER ;
+
+CALL udp_graduate_all_students_by_year(2017);
