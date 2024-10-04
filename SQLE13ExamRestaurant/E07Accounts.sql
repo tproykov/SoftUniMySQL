eb8c53390436fc3817c0b6b02ad5d43881706495
Todor Proykov <tproykov@gmail.com>
2024-10-05 00:05:17 +0100

    Update E07Accounts.sql

---
+++ SQLE13ExamRestaurant/E07Accounts.sql
@@ -0,0 +1,7 @@
+SELECT
+    CONCAT(last_name, first_name, CHARACTER_LENGTH(first_name), 'Restaurant') AS username,
+    REVERSE(SUBSTRING(email, 2, 13)
+FROM waiters
+ORDER BY password;
+
+USE restaurant;
